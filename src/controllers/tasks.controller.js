e162c68395df4e842d5a68314c83456559e54039
ashraful <ashraful.islam143084@gmail.com>
2025-04-19 23:39:38 +0600

    update

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,29 +1,13 @@
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
-const { createTaskProvider } = require("../providers/task.provider");
+const {
+  createTaskProvider,
+  getTaskProvider,
+  updateTaskProvider,
+} = require("../providers/task.provider");
 
-const handleGetTasks = (req, res) => {
-  let response = [
-    {
-      title: "Task Title 1",
-      description: "This is the description of this tasks",
-      priority: "high",
-      status: "todo",
-    },
-    {
-      title: "Task Title 2",
-      description: "This is the description of this tasks",
-      priority: "high",
-      status: "todo",
-    },
-    {
-      title: "Task Title 3",
-      description: "This is the description of this tasks",
-      priority: "high",
-      status: "todo",
-    },
-  ];
-
-  res.status(StatusCodes.OK).json(response);
+const handleGetTasks = async (req, res) => {
+  const tasks = await getTaskProvider(req, res);
+  res.status(StatusCodes.OK).json(tasks);
 };
 
 const handlePostTasks = async (req, res) => {
@@ -31,8 +15,10 @@ const handlePostTasks = async (req, res) => {
   res.status(StatusCodes.CREATED).json(task);
 };
 
-const handlePatchTasks = (req, res) => {
-  res.send("This is patch task controller");
+const handlePatchTasks = async (req, res) => {
+  const updatedTask = await updateTaskProvider(req, res);
+
+  res.status(StatusCodes.OK).json(updatedTask);
 };
 
 const handleDeleteTasks = (req, res) => {
--- src/providers/task.provider.js
+++ src/providers/task.provider.js
@@ -12,4 +12,21 @@ const createTaskProvider = async (req, res) => {
   return await task.save();
 };
 
-module.exports = { createTaskProvider };
+const getTaskProvider = async (req, res) => {
+  const tasks = await Task.find();
+  return tasks;
+};
+
+const updateTaskProvider = async (req, res) => {
+  const task = await Task.findById(req.body["_id"]);
+
+  task.title = req.body.title;
+  task.description = req.body.description;
+  task.status = req.body.status;
+  task.priority = req.body.priority;
+  task.dueDate = req.body.dueDate;
+
+  return await task.save();
+};
+
+module.exports = { createTaskProvider, getTaskProvider, updateTaskProvider };
